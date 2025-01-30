88d9af8c09ab4e835cc073690705e628eee70eea
Abdul Keita <akk6047@psu.edu>
2025-01-30 11:50:57 -0500

    index.js

--- Activities/index.js
+++ Activities/index.js
@@ -1,1 +1,18 @@
-console.log("hello world!")
+const score = 100
+const temperature = 36.6
+const greeting = "Hello, JavaScript!"
+const isLearningFun = true
+console.log(score)
+console.log(temperature)
+console.log(greeting)
+console.log(isLearningFun)
+
+//using const because the object shouldn't be changed, but we can add properties
+//const uses key value so it looks like name: "Dul"
+// function uses this.varName = ___
+const student = {
+    name: "Dul",
+    age: 21,
+    subjects: ["math","science","econ"],
+    isGraduated: false
+}
