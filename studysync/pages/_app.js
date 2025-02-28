2a67a07a8d54f28de9fe8274e7ce6dee1d203ab7
Abdul Keita <akk6047@psu.edu>
2025-02-28 14:13:52 -0500

    rough draft

--- studysync/pages/_app.js
+++ studysync/pages/_app.js
@@ -1,5 +1,26 @@
-import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+import { createGlobalStyle } from "styled-components";
+
+const poppins = Poppins({
+  weight: ["400", "700"], // Normal (400) & Bold (700)
+  subsets: ["latin"], // Supports Latin characters
+  variable: "--font-poppins", // CSS Variable
+});
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: var(--font-poppins), sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </div>
+  );
 }
+
