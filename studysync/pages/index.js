cebdefdb1de83632257e743e558ef9dd809cfa18
Abdul Keita <akk6047@psu.edu>
2025-03-01 01:11:59 -0500

    finished continue button

--- studysync/pages/index.js
+++ studysync/pages/index.js
@@ -42,11 +42,36 @@ export default function Home() {
   const [error, setError] = useState("");
   const psuEmailRegex = /^[a-z]{3}[0-9]{4}@psu\.edu$/;
 
-  const ContinueClick = () => {
-    if (email.length != 15) {
-      setError("Please enter email in proper abc1234@gmail.com");
+  //keeps track of the input being passed through out main input box, if it's empty it doesn't display any error messages
+  const inputResponse = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+
+    // Clear error if input is empty
+    if (value.trim() === "") {
+      setError("");
+    }
+  };
+
+  //when enter is clicked for the input or the Continue button is clicked, this checks to make sure we have a non-blank psu email.
+  // If we do then we continue with the sign up process
+  const continueSignUp = () => {
+    // making sure we don't have a blank email trying to be passed through
+    if (email.trim === "") {
+      setError("Email can not be blank!");
+    }
+    //testing the email to see if it's in psu format, if it is then we negate it to skip the if statement, if it isn't then we setError equal to the text
+    else if (!psuEmailRegex.test(email)) {
+      setError("Your email is not in abc1234@psu.com format");
     } else {
       setError("");
+      setState("SignUp");
+    }
+  };
+  // if we hit enter in the input box it takes us to the continueSignUp event handler
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      continueSignUp();
     }
   };
   return (
@@ -58,6 +83,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/*The initial state is the state we load into, from here we can eithe go to signing the user up or logging in with prior credentials*/}
       {state === "Initial" && (
         <>
           <PageWrapper>
@@ -67,15 +93,42 @@ export default function Home() {
                 <p>First, enter your Penn State email</p>
                 <Inputs
                   placeholder="abc1234@psu.edu"
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={inputResponse}
+                  onKeyDown={handleEnter}
                 />
                 {error && <ErrorMessage>{error}</ErrorMessage>}
-                <button onClick={ContinueClick}>Continue</button>
+                <button onClick={continueSignUp}>Continue</button>
                 <h1>OR</h1>
                 <button>Log In</button>
               </SignInDiv>
             </main>
           </PageWrapper>
+        </>
+      )}
+      {state === "SignUp" && (
+        <>
+          <>
+            <PageWrapper>
+              <main>
+                <PoppinsHead>Finish Signing up StudySync</PoppinsHead>
+                <SignInDiv>
+                  <p>
+                    Enter the basic information below to revolutionize your
+                    studying!
+                  </p>
+                  <Inputs
+                    placeholder="abc1234@psu.edu"
+                    onChange={inputResponse}
+                    onKeyDown={handleEnter}
+                  />
+                  {error && <ErrorMessage>{error}</ErrorMessage>}
+                  <button onClick={continueSignUp}>Continue</button>
+                  <h1>OR</h1>
+                  <button>Log In</button>
+                </SignInDiv>
+              </main>
+            </PageWrapper>
+          </>
         </>
       )}
     </> //end
