01e952af44f293653a90efd76859d3a394821c63
Abdul Keita <akk6047@psu.edu>
2025-03-05 05:53:48 -0500

    filler

---
+++ studysync/pages/contact.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <main style={{ display: 'grid', placeItems: 'center', height: '100vh', textAlign: 'center' }}>
+        <div>
+          <h1>About Page</h1>
+          <p>Welcome to the About page. More content coming soon!</p>
+        </div>
+      </main>
+    </>
+  );
+}
